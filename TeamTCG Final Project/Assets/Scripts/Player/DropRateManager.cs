1d20544bf8bdbb96a82dbc7c19c8698e3140487c
Krampuso <al849783@ucf.edu>
2025-04-05 21:32:53 -0400

    update

---
+++ TeamTCG Final Project/Assets/Scripts/Player/DropRateManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropRateManager : MonoBehaviour
+{
+    [System.Serializable]
+
+    public class Drops 
+    {
+        public string name;
+        public GameObject itemPrefab;
+        public float dropRate;
+    }
+    public List<Drops> drops;
+
+    void OnDestroy()
+    {
+        float randomNumer = Random.Range(0f, 100f);
+        List<Drops> possibleDrops = new List<Drops>();
+
+
+        foreach (Drops rate in drops)
+        {
+           if(randomNumer <= rate.dropRate)
+           {
+             possibleDrops.Add(rate);
+           } 
+        }
+        //Check if there are possible drops
+        if(possibleDrops.Count > 0)
+        {
+            Drops drops = possibleDrops(Random.Range(0, possibleDrops.Count));
+            Instantiate(rate.itemPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
