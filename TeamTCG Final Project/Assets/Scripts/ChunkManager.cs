1d5cdd0ef54ec0824f12d91e3bf4a7c698277b42
BilboBallins101 <drns14@aol.com>
2025-04-06 21:38:50 -0400

    Starting level generation

---
+++ TeamTCG Final Project/Assets/Scripts/ChunkManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkManager : MonoBehaviour
+{
+    public GameObject chunkPrefab;
+    public Transform player;
+    public int chunkRadius = 2; // How many chunks around the player
+    public Vector2 chunkSize = new Vector2(5f, 5f); // X and Z size of one chunk
+
+    private Dictionary<Vector2Int, GameObject> activeChunks = new Dictionary<Vector2Int, GameObject>();
+    private Vector2Int currentPlayerChunk;
+
+    void Start()
+    {
+        UpdateChunks(true);
+    }
+
+    void Update()
+    {
+        Vector2Int newPlayerChunk = GetPlayerChunkCoord();
+        if (newPlayerChunk != currentPlayerChunk)
+        {
+            Debug.Log($"Player moved to chunk: {newPlayerChunk}");
+            currentPlayerChunk = newPlayerChunk;
+            UpdateChunks();
+        }
+    }
+
+    Vector2Int GetPlayerChunkCoord()
+    {
+        int x = Mathf.FloorToInt(player.position.x / chunkSize.x);
+        int z = Mathf.FloorToInt(player.position.z / chunkSize.y);
+        return new Vector2Int(x, z);
+    }
+
+    void UpdateChunks(bool forceInit = false)
+    {
+        HashSet<Vector2Int> newChunks = new HashSet<Vector2Int>();
+
+        for (int x = -chunkRadius; x <= chunkRadius; x++)
+        {
+            for (int z = -chunkRadius; z <= chunkRadius; z++)
+            {
+                Vector2Int chunkCoord = currentPlayerChunk + new Vector2Int(x, z);
+                newChunks.Add(chunkCoord);
+
+                if (!activeChunks.ContainsKey(chunkCoord))
+                {
+                    Vector3 position = new Vector3(
+                        chunkCoord.x * chunkSize.x,
+                        0f,
+                        chunkCoord.y * chunkSize.y
+                    );
+                    GameObject newChunk = Instantiate(chunkPrefab, position, Quaternion.identity);
+                    activeChunks.Add(chunkCoord, newChunk);
+                }
+            }
+        }
+
+        // Clean up chunks that are no longer in range
+        List<Vector2Int> toRemove = new List<Vector2Int>();
+        foreach (var kvp in activeChunks)
+        {
+            if (!newChunks.Contains(kvp.Key))
+            {
+                Destroy(kvp.Value);
+                toRemove.Add(kvp.Key);
+            }
+        }
+
+        foreach (var key in toRemove)
+        {
+            activeChunks.Remove(key);
+        }
+    }
+}
